903984c27ac0eb97b82ab983b291c80e47e07b3a
BabishaSumi <babishasumi@outlook.com>
2024-03-24 23:42:33 +0530

    whatsapp icon & hidden navbar

--- chasmish-app/src/app/app.component.ts
+++ chasmish-app/src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { WhatsappIconComponent } from './whatsapp-icon/whatsapp-icon.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent,FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent, AboutComponent, WhatsappIconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
