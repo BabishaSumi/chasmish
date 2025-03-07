95487b0d25ddc05bfd4bcff452c82cfeb4076d70
BabishaSumi <babishasumi@outlook.com>
2025-03-07 23:28:03 +0530

    Change Firebase redirection to use angular instead

--- chasmish-app/src/app/app.routes.ts
+++ chasmish-app/src/app/app.routes.ts
@@ -7,6 +7,6 @@ export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'faq', component: FAQComponent},
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: '**', component: HomeComponent} // replace with 404
 ] ; 
