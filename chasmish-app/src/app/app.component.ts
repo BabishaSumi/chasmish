5011f7a5742ce343ca2fe62a6a3ad065f7e44278
BabishaSumi <babishasumi@outlook.com>
2025-03-08 17:34:16 +0530

    clean up

--- chasmish-app/src/app/app.component.ts
+++ chasmish-app/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
 import { WhatsappIconComponent } from './whatsapp-icon/whatsapp-icon.component';
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent, AboutComponent, WhatsappIconComponent],
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, WhatsappIconComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
--- chasmish-app/src/app/footer/footer.component.ts
+++ chasmish-app/src/app/footer/footer.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
     styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+    currentYear: number = new Date().getFullYear();
 }
